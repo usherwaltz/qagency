3f0222d1c150cafaf3dd3ccbf747807acb25540c
usherwaltz <81054824+usherwaltz@users.noreply.github.com>
2024-09-11 12:55:51 +0200

    Dio Setup (#4)
    If applied, this commit will setup the Dio Client and Interceptors

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,3 +1,4 @@
+export 'api_response/api_response_model.dart';
 export 'genre/genre_model.dart';
 export 'genres_list_response/genres_list_response_model.dart';
 export 'movie/movie_model.dart';
--- lib/repositories/movies/movies_repository.dart
+++ lib/repositories/movies/movies_repository.dart
@@ -1,26 +1,33 @@
-import 'package:dio/dio.dart';
-import 'package:qagency/dio/dio_client.dart';
-
+import '../../dio/dio_client.dart';
+import '../../dio/endpoints/endpoints.dart';
 import '../../models/models.dart';
 
 class MoviesRepository {
-  static final Dio _dio = DioClient().dio;
+  static final DioClient _dio = DioClient();
 
   Future<MoviesListResponseModel> getPopular({
     required int page,
   }) async {
-    final result = await _dio.get(
-      'https://api.themoviedb.org/3/movie/popular?language=en_US&page=$page',
+    final apiResponse = await _dio.get(
+      MovieEndpoints.popular(page),
     );
 
-    return MoviesListResponseModel.fromJson(result.data);
+    if (apiResponse.isSuccessful) {
+      return MoviesListResponseModel.fromJson(apiResponse.data);
+    }
+
+    return const MoviesListResponseModel();
   }
 
   Future<List<GenreModel>> getGenres() async {
-    final result = await _dio.get(
-      'https://api.themoviedb.org/3/genre/movie/list',
+    final apiResponse = await _dio.get(
+      MovieEndpoints.genres,
     );
 
-    return GenresListResponseModel.fromJson(result.data).genres;
+    if (apiResponse.isSuccessful) {
+      return GenresListResponseModel.fromJson(apiResponse.data).genres;
+    }
+
+    return [];
   }
 }
